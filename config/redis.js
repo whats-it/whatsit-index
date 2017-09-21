f092b715118094ca2731ac4c9bbe964ce828438e
kyewon <k@bluehack.net>
2017-09-21 22:40:38 +0900

    add DATA_SET_ID env

--- config/redis.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-var config = {
-    port  : 6379  , // Port of your locally running Redis server
-    host  : '35.197.141.242',
-    //host    : '127.0.0.1',
-    //scope : 'demo'  // Use a scope to prevent two NRPs from sharing messages
-};
-exports.connector = config;
